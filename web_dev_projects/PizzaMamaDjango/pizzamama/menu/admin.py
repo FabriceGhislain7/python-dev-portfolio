15b1d7cdc8b69846694927136a00c900c7ce82b1
Fabrice Ghislain Tebou <ghislaintebou@mail.com>
2025-04-15 22:11:25 +0200

    Project PizzaMamaDjango

--- web_dev_projects/PizzaMamaDjango/pizzamama/menu/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Pizza
-
-class PizzaAdmin(admin.ModelAdmin):
-    list_display = ('name', 'ingredients', 'vegetarian', 'price')
-    search_fields = ["name"]
-
-admin.site.register(Pizza, PizzaAdmin)
